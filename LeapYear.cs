b21c24bb278c912eebcd8d7f696cbc2930ef4194
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-15 12:49:10 +0530

    [Dhanya R Hegde] : Added LeapYear Problem

---
+++ LeapYear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProblems
+{
+    internal class LeapYear
+    {
+        public void leapYear(int year)
+        {
+            if (year % 400 == 0) Console.WriteLine("The given year is a Leap year");
+            else if (year % 100 == 0) Console.WriteLine("The given year is not a Leap year");
+            else if (year % 4 == 0) Console.WriteLine("The given year is a Leap year");
+            else Console.WriteLine("The given year is not a Leap year");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             
-            FlipCoin flipCoin = new FlipCoin();
-            flipCoin.headTailPercentage(8);
+            //FlipCoin flipCoin = new FlipCoin();
+            //flipCoin.headTailPercentage(8);
+
+            LeapYear leapYear = new LeapYear();
+            leapYear.leapYear(2004);
         }
     }
 }
