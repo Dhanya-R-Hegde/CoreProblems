9fa5ffd5c10d4343e92461e44b8548e119ab228a
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-15 12:56:24 +0530

    [Dhanya R Hegde] : Added PowerOfTwo Problem

---
+++ PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProblems
+{
+    internal class PowerOfTwo
+    {
+        public int n;
+        public PowerOfTwo(int n)
+        {
+            this.n = n;
+        }
+
+        public void powerOfTwo()
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write(Math.Pow(2, i) + "  ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
-            //FlipCoin flipCoin = new FlipCoin();
-            //flipCoin.headTailPercentage(8);
+
+            FlipCoin flipCoin = new FlipCoin();
+            flipCoin.headTailPercentage(8);
 
             LeapYear leapYear = new LeapYear();
             leapYear.leapYear(2004);
+
+            PowerOfTwo powerOfTwo = new PowerOfTwo(10);
+            powerOfTwo.powerOfTwo();
+
         }
     }
 }
